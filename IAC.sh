dab9a7a7dbb57eb9684f465b2a0a197aec18684e
yuri <117326944+lswhereami@users.noreply.github.com>
2022-11-06 20:18:51 -0300

    Update IAC.sh

--- IAC.sh
+++ IAC.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo 'criando usuarios'
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd 123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd 123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd 123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd 123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd 123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd 123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd 123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd 123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd 123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt 123) -G GRP_SE
 
 
 echo 'criando permissões de diretorios'
